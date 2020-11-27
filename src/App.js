af603cb09cb8c7eff143f0be7b8421540325aad1
ynaraoliveira <ynaraoliveira.s@gmail.com>
2020-11-27 17:11:49 -0300

    cria mock com vagas

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import logo from './assets/images/logo.svg';
-import { Header, JobCard } from './components'
-import './styles/index.scss';
+import React from "react";
+import logo from "./assets/images/logo.svg";
+import { Header, JobCard } from "./components";
+import "./styles/index.scss";
+import jobList from "./_mocks/jobList";
 
 function App() {
   return (
     <div className="App">
       <Header logo={logo} showBackButton showExitButton />
-      <JobCard BadgesList = {["Leblon", "CLT", "sem experiência"]} Title = "Assistente de Criação" jobImage = "https://picsum.photos/100/100?random=2" jobDescription = 'Empresa XPTO procura assistente de criação para atuar em agência. '/>
+      <JobCard
+        BadgesList={["Leblon", "CLT", "sem experiência"]}
+        Title="Assistente de Criação"
+        jobImage="https://picsum.photos/100/100?random=2"
+        jobDescription="Empresa XPTO procura assistente de criação para atuar em agência. "
+      />
     </div>
   );
 }
---
+++ src/_mocks/jobList.js
@@ -0,0 +1,28 @@
+const jobList = [
+  {
+    id: 1,
+    badgesList: ["Leblon", "CLT", "sem experiência"],
+    title: "Assistente de Criação",
+    jobImage: "https://picsum.photos/100/100?random=1",
+    jobDescription:
+      "Empresa XPTO procura assistente de criação para atuar em agência.",
+  },
+  {
+    id: 2,
+    badgesList: ["Leblon", "CLT", "sem experiência"],
+    title: "Assistente de Criação",
+    jobImage: "https://picsum.photos/100/100?random=2",
+    jobDescription:
+      "Empresa XPTO procura assistente de criação para atuar em agência.",
+  },
+  {
+    id: 3,
+    badgesList: ["Leblon", "CLT", "sem experiência"],
+    title: "Assistente de Criação",
+    jobImage: "https://picsum.photos/100/100?random=3",
+    jobDescription:
+      "Empresa XPTO procura assistente de criação para atuar em agência.",
+  },
+];
+
+export default jobList;
