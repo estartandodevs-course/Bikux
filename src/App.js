07535b842077e6d954584d099e5ce323b7a4a119
Ana Araujo <anapro1141@gmail.com>
2020-11-27 10:01:33 -0300

    estiliza navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-<<<<<<< HEAD
 import React from 'react'
-import NavBar from './components/BottomTabNavigator/BottomTabNavigator'
 import './App.css';
-=======
 import logo from './assets/images/logo.svg';
 import { Header } from './components'
+import  BottomNavBar  from './components/BottomNavBar/BottomNavBar'
 import './styles/index.scss';
->>>>>>> c41accb02a511c9b6d9711528feb3abf5d779d48
+
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <NavBar />
-=======
+
+     
       <Header logo={logo} showBackButton showExitButton />
->>>>>>> c41accb02a511c9b6d9711528feb3abf5d779d48
+
+      <BottomNavBar showAddVagaButton showHomeButton showSearchButton showNotificacionButton/>
+
     </div>
   );
 }
