a8895cc113471a7cd58f00980b86a7b1c74f3131
Ana Araujo <anapro1141@gmail.com>
2020-12-04 17:21:44 -0300

    estiliza input field

--- src/App.js
+++ src/App.js
@@ -5,17 +5,37 @@ import "./styles/index.scss";
 import { Vaga } from "./pages/Vaga/Vaga";
 import { Feed } from "./pages/Feed/Feed";
 import { Search } from "./pages/Search/Search";
+import { JobDetailsPage } from "./pages/JobDetailsPage/JobDetailsPage";
 import BottomNavBar from "./components/BottomNavBar/BottomNavBar";
-import { InputField } from "./components/inputField/inputField";
+import NoMatch from "./pages/NoMatch/NoMatch"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export function App() {
   return (
-  <div>
+    <Router>
       <Header logo={logo} showBackButton showExitButton />
-      <InputField type="text" placeholder="Insira seu nome" required="required" label="Nome"/>
-      
-      </div>    
+      <Switch>
+        <Route path="/" exact>
+          <Feed />
+        </Route>
+        <Route path="/vaga">
+          <Vaga />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+        <Route path="/detalhes/vaga/:indexOftoBeSaw">
+          <JobDetailsPage />
+        </Route>
+        <Route component={NoMatch}></Route>
+      </Switch>
+      <BottomNavBar
+        showAddVagaButton
+        showHomeButton
+        showSearchButton
+        showNotificacionButton
+      />
+    </Router>
   );
 }
 
--- src/components/inputField/inputField.js
+++ src/components/inputField/inputField.js
@@ -1,21 +1,32 @@
 import React from 'react'
+import { useState } from 'react';
 import './inputField.scss'
 
 export function InputField(props) {
-    const {label, type, placeholder, required, size} = props;   
-    const ast = '*'                 
-    const labelRequired = required === "required" ? `${ast}`: "";
+    const {label, type, placeholder, required, height, width} = props;                  
+    const labelRequired = required === "required" ? '*' : "";
+    const [value, setValue] = useState("");
 
+    function onChange (event) {
+        setValue(event.target.value);
+    }
+   
     return (
-        <div className="input-text">
-            <label className="label">{label}<span className="required">{labelRequired}</span></label>
-            <input  className="input" 
-            type={type} 
-            placeholder={placeholder} 
-            required={required}>
-            </input>
-          
+        <div className="input-container">
+            <div class="input-field">
+                <label>{label}<span className="required">{labelRequired}</span></label>
+                <input 
+                style={{
+                    height: height,
+                    width: width
+                }} 
+                onChange={onChange}
+                type={type} 
+                placeholder={placeholder} 
+                required={required}>
+                </input>
+            </div>
         </div>
-
     )
 }
+
