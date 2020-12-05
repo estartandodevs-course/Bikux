66697058b49809ffa59ab3e6edc936666cb02f8b
Ana Araujo <anapro1141@gmail.com>
2020-12-05 19:09:50 -0300

    add app js

--- src/App.js
+++ src/App.js
@@ -2,24 +2,40 @@ import React from "react";
 import logo from "./assets/images/logo.svg";
 import { Header } from "./components";
 import "./styles/index.scss";
-import TextArea from "./components/TextArea/TextArea"
-/*import { Vaga } from "./pages/Vaga/Vaga";
+import { Vaga } from "./pages/Vaga/Vaga";
 import { Feed } from "./pages/Feed/Feed";
 import { Search } from "./pages/Search/Search";
 import { JobDetailsPage } from "./pages/JobDetailsPage/JobDetailsPage";
 import BottomNavBar from "./components/BottomNavBar/BottomNavBar";
 import NoMatch from "./pages/NoMatch/NoMatch"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";*/
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export function App() {
   return (
-    <TextArea
-    label="Detalhes da vaga" 
-    placeholder="Ex.: Procuramos caixa para o período da manhã, 
-    não necessita experiência.
-    Oferecemos salário compatível,VA, VT, AM, AO.
-    Desejável que more próximo."
-    />
+    <Router>
+      <Header logo={logo} showBackButton showExitButton />
+      <Switch>
+        <Route path="/" exact>
+          <Feed />
+        </Route>
+        <Route path="/vaga">
+          <Vaga />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+        <Route path="/detalhes/vaga/:indexOftoBeSaw">
+          <JobDetailsPage />
+        </Route>
+        <Route component={NoMatch}></Route>
+      </Switch>
+      <BottomNavBar
+        showAddVagaButton
+        showHomeButton
+        showSearchButton
+        showNotificacionButton
+      />
+    </Router>
   );
 }
 
