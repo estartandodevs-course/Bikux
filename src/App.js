3339f926112901b896c3516bece8be4d3f9b5544
Ana Araujo <anapro1141@gmail.com>
2020-12-05 19:39:07 -0300

    faz map nas options

--- src/App.js
+++ src/App.js
@@ -2,42 +2,40 @@ import React from "react";
 import logo from "./assets/images/logo.svg";
 import { Header } from "./components";
 import "./styles/index.scss";
-import SelectBox from "./components/SelectBox/SelectBox"
-/*import { Vaga } from "./pages/Vaga/Vaga";
+import { Vaga } from "./pages/Vaga/Vaga";
 import { Feed } from "./pages/Feed/Feed";
 import { Search } from "./pages/Search/Search";
 import { JobDetailsPage } from "./pages/JobDetailsPage/JobDetailsPage";
 import BottomNavBar from "./components/BottomNavBar/BottomNavBar";
 import NoMatch from "./pages/NoMatch/NoMatch"
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";*/
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export function App() {
-
-  
   return (
-    <SelectBox
-    label="Detalhes da vaga" 
-    options = {[
-      {
-        label: "Vamos sugerir vagas pensando nisso",
-        value: "",
-      },
-      {
-        label: "Mango",
-        value: "mango",
-      },
-      {
-        label: "Banana",
-        value: "banana",
-      },
-      {
-        label: "Pineapple",
-        value: "pineapple",
-      },
-    ]}
-      
-  
-    />
+    <Router>
+      <Header logo={logo} showBackButton showExitButton />
+      <Switch>
+        <Route path="/" exact>
+          <Feed />
+        </Route>
+        <Route path="/vaga">
+          <Vaga />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+        <Route path="/detalhes/vaga/:indexOftoBeSaw">
+          <JobDetailsPage />
+        </Route>
+        <Route component={NoMatch}></Route>
+      </Switch>
+      <BottomNavBar
+        showAddVagaButton
+        showHomeButton
+        showSearchButton
+        showNotificacionButton
+      />
+    </Router>
   );
 }
 
--- src/components/SelectBox/SelectBox.js
+++ src/components/SelectBox/SelectBox.js
@@ -2,16 +2,19 @@ import React from 'react'
 import './SelectBox.scss'
 
 function SelectBox(props) {
-    const {label, options = [], height, width, onChange} = props;  
+    const {label, options = [], required, height, width, onChange} = props; 
+    const labelRequired = required ? '*' : "";
+
     return (
         <div className="select-container">
             <div className ="select-box">
-                <label>{label}</label>
+                <label>{label}<span className="required">{labelRequired}</span></label>
                 <select
                 style={{
                     height: height,
                     width: width
                 }} 
+                className="select"
                 onChange={onChange}
                 >
                     {options.map((option) => (
